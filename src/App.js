343cd8f61830549ad8d64036b2882eabbed012f3
bezlin6mechminerz <54895485+bezlin6mechminerz@users.noreply.github.com>
2021-04-17 23:49:42 +0530

    he

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Landing extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="landingpagecss">
+        <h1>Sign In</h1>
+        <button className="langingpagebuttons">Sign Up</button>
+        <br />
+        <button>Login</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Landing;
